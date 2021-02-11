46beea4ea293cf0a74eb63c479d39c0bfd4f19d1
fyn5908 <qwe7951384261@outlook.com>
2021-02-11 19:28:29 +0800

    page 4

---
+++ 第4页质数.hs
@@ -0,0 +1,15 @@
+prime_number_1 x = primeNumber (read x) ::Int 
+
+primeNumber n 
+    |n == 1 = error "不能等于1"
+    |n mod 2 == 1 = print "False"
+    |3 > (n-1) = print "Ture"
+    |otherwise = print "False"
+
+
+
+main = do
+    print "输入n"
+    number_1 <- getLine
+    prime_number_1 number_1
+    
