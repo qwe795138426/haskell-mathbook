f6e10b0c0d35a6f23a243d2115b284bce2012d44
fyn5908 <qwe7951384261@outlook.com>
2021-02-12 09:04:26 +0800

    page 4

--- 第4页质数.hs
+++ 第4页质数.hs
@@ -1,13 +1,16 @@
-primeNumber :: (Eq a, Fractional a) => a -> IO ()
+primeNumber :: Double  -> Bool
 primeNumber x
     |x==1 = error "Can't be equal to 1"
-    |(x/2)==0 || (x/3)==0 || (x/5)==0 = print "False" 
-    |otherwise = print "True"
+    |x < 0 = error "It can't be negative"
+    |floor (x/2) < ceiling (x/2) || floor (x/3) < ceiling (x/3) || floor (x/3) < ceiling (x/3) = False 
+    |otherwise = False 
+
 
 
 main :: IO ()
 main = do
     print "n"
     num_1 <- getLine
-    let num_2 = read num_1 ::Integer
-    print "total"
+    let num_2 = read num_1 ::Double
+    let ansewer = primeNumber num_2
+    print ansewer
