c9324963d5b4416b1ad97aaacfbb696d3f22bfb3
fyn5908 <qwe7951384261@outlook.com>
2021-04-24 08:45:33 +0800

    taile

--- special_vision:Taile/taile.hs
+++ special_vision:Taile/taile.hs
@@ -1,5 +1,23 @@
+main :: IO ()
 main = do 
-    putStrLn "testing release"
+    putStrLn "testing release(DANGEROUS)"
     putStrLn "powered by haskell"
     putStrLn "it is designed for ex"
-    putStrLn "Please input your "
+    putStrLn "Please input a Index term"
+    a <- getLine
+    putStrLn "Please input factorial number"
+    b <- getLine
+    let a_1=read a :: Double
+    let b_1=read b :: Integer
+    let answer= taileEx a_1 b_1
+    print answer
+
+
+
+taileEx :: Fractional a => a -> Integer -> a
+taileEx a 0 = a
+taileEx a b = a^b/fromInteger(factorial b) + taileEx a b-1
+ 
+factorial :: (Eq p, Num p) => p -> p
+factorial 0 = 1   
+factorial n = n * factorial (n - 1)  
